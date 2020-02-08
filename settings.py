e194a9a9d620154dc9292fd8705cc1fa34751059
flokinagee <59611859+flokinagee@users.noreply.github.com>
2020-02-08 13:15:48 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,3 +5,6 @@ os.environ['password'] = "dbpassword"
 os.environ['cluster_endpoint'] = "docdb-2020-02-01-05-52-57.cluster-ced90qgfa8hd.ap-southeast-1.docdb.amazonaws.com"
 os.environ['port'] = "27017"
 #os.environ['log_dir'] = "./log/flask-crud-app"
+
+# Install community version locally
+#https://github.com/mongodb/mongo/wiki/Build-Mongodb-From-Source
